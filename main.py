d15d8fe04d24d089c66473e38d111af20a97f9d7
max <m7a7x7gg@gmail.com>
2023-04-20 20:24:36 +0300

    task2

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from datetime import datetime
 from data import db_session
+from data.users import User
+from data.jobs import Jobs
+from flask import Flask, render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -7,7 +10,7 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/blogs.db")
-    app.run()
+    app.run(port=8080, host='127.0.0.1')
 
 
 if __name__ == '__main__':
